ff8539216ed4637c054e27ef8deeafe6d34f796b
Ivan Pankov <ivanpankov@abv.bg>
2016-11-09 09:14:35 +0200

    working process

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var config = require('./config');
@@ -17,6 +18,7 @@ db.once('open', function () {
 var app = express();
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(passport.initialize());
 
 app.get('*', (req, res, next) => {
@@ -73,6 +75,54 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRed
     }
 );
 
+app.post('/auth/local/register', function (req, res) {
+    var password = req.body.password;
+    var newUser = new User({
+        username: req.body.username,
+        firstname: req.body.firstname || '',
+        lastname: req.body.lastname || ''
+    });
+    var register = function(err, user) {
+        if (err) {
+            return res.status(500).json({ err: err });
+        }
+        passport.authenticate('local')(req, res, function () {
+            return res.status(200).json({ status: 'Registration Successful!' });
+        });
+    };
+
+    User.register(newUser, password, register);
+});
+
+app.post('/auth/local', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+
+        if (!user) {
+            return res.status(401).json({
+                err: info
+            });
+        }
+
+        req.logIn(user, function (err) {
+            if (err) {
+                return res.status(500).json({
+                    err: 'Could not log in user'
+                });
+            }
+
+            var token = Verify.getToken(user);
+
+            res.status(200).json({
+                status: 'Login successful!',
+                success: true,
+                token: token
+            });
+        });
+    })(req, res, next);
+});
 
 
 var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
